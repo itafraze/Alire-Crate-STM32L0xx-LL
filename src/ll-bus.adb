ea8c2e6b26665a81091d97ce62fcbf551cb4ccfa
Emanuele Zarfati <zarfati.tecno@gmail.com>
2024-03-09 23:51:29 +0100

    Implement `APB1_GRP1_Enable_Clock` and `IOP_GRP1_EnableClock`of `Bus` driver

---
+++ src/ll-bus.adb
@@ -0,0 +1,142 @@
+------------------------------------------------------------------------------
+--  Copyright 2024, Emanuele Zarfati
+--
+--  Licensed under the Apache License, Version 2.0 (the "License");
+--  you may not use this file except in compliance with the License.
+--  You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+--  Unless required by applicable law or agreed to in writing, software
+--  distributed under the License is distributed on an "AS IS" BASIS,
+--  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--  See the License for the specific language governing permissions and
+--  limitations under the License.
+--
+------------------------------------------------------------------------------
+--
+--  Revision History:
+--    2024.03 E. Zarfati
+--       - First version
+--
+------------------------------------------------------------------------------
+
+with CMSIS.Device;
+with CMSIS.Device.RCC;
+   use CMSIS.Device.RCC;
+
+package body LL.Bus is
+   --  Bus low-level driver body
+   --
+   --  Implementation notes:
+   --  - Based on source files:
+   --    - stm32l0xx_hal_driver:Inc/stm32l0xx_ll_bus.h
+
+   ---------------------------------------------------------------------------
+   procedure APB1_GRP1_Enable_Clock (
+      Peripherals : APB1_GRP1_Peripheral_Selection_Type) is
+   --
+   --  TODO:
+   --  - Device-category-dependent implementation to handle missing
+   --    peripherals
+      UNUSED_Bit : CMSIS.Device.Bit
+         with Volatile;
+   begin
+
+      for Ph in APB1_GRP1_Peripheral_Selection_Type'Range
+      loop
+         if Peripherals (Ph) = True
+         then
+            case Ph is
+               when TIM2 =>
+                  RCC_Periph.APB1ENR.TIM2EN := 2#1#;
+                  UNUSED_Bit := RCC_Periph.APB1ENR.TIM2EN;
+               when TIM3 =>
+                  RCC_Periph.APB1ENR.TIM3EN := 2#1#;
+                  UNUSED_Bit := RCC_Periph.APB1ENR.TIM3EN;
+               when TIM6 =>
+                  RCC_Periph.APB1ENR.TIM6EN := 2#1#;
+                  UNUSED_Bit := RCC_Periph.APB1ENR.TIM6EN;
+               when TIM7 =>
+                  RCC_Periph.APB1ENR.TIM7EN := 2#1#;
+                  UNUSED_Bit := RCC_Periph.APB1ENR.TIM7EN;
+               when LCD => null;
+               when WWDG =>
+                  RCC_Periph.APB1ENR.WWDGEN := 2#1#;
+                  UNUSED_Bit := RCC_Periph.APB1ENR.WWDGEN;
+               when SPI2 =>
+                  RCC_Periph.APB1ENR.SPI2EN := 2#1#;
+                  UNUSED_Bit := RCC_Periph.APB1ENR.SPI2EN;
+               when USART2 =>
+                  RCC_Periph.APB1ENR.USART2EN := 2#1#;
+                  UNUSED_Bit := RCC_Periph.APB1ENR.USART2EN;
+               when LPUART1 =>
+                  RCC_Periph.APB1ENR.LPUART1EN := 2#1#;
+                  UNUSED_Bit := RCC_Periph.APB1ENR.LPUART1EN;
+               when USART4 =>
+                  RCC_Periph.APB1ENR.USART4EN := 2#1#;
+                  UNUSED_Bit := RCC_Periph.APB1ENR.USART4EN;
+               when USART5 =>
+                  RCC_Periph.APB1ENR.USART5EN := 2#1#;
+                  UNUSED_Bit := RCC_Periph.APB1ENR.USART5EN;
+               when I2C1 =>
+                  RCC_Periph.APB1ENR.I2C1EN := 2#1#;
+                  UNUSED_Bit := RCC_Periph.APB1ENR.I2C1EN;
+               when I2C2 =>
+                  RCC_Periph.APB1ENR.I2C2EN := 2#1#;
+                  UNUSED_Bit := RCC_Periph.APB1ENR.I2C2EN;
+               when USB => null;
+               when CRS => null;
+               when PWR =>
+                  RCC_Periph.APB1ENR.PWREN := 2#1#;
+                  UNUSED_Bit := RCC_Periph.APB1ENR.PWREN;
+               when DAC1 => null;
+               when I2C3 =>
+                  RCC_Periph.APB1ENR.I2C3EN := 2#1#;
+                  UNUSED_Bit := RCC_Periph.APB1ENR.I2C3EN;
+               when LPTIM1 =>
+                  RCC_Periph.APB1ENR.LPTIM1EN := 2#1#;
+                  UNUSED_Bit := RCC_Periph.APB1ENR.LPTIM1EN;
+            end case;
+         end if;
+      end loop;
+
+   end APB1_GRP1_Enable_Clock;
+
+   ---------------------------------------------------------------------------
+   procedure IOP_GRP1_EnableClock (
+      Peripherals : IOP_GRP1_Peripheral_Selection_Type) is
+   --
+      UNUSED_Bit : CMSIS.Device.Bit
+         with Volatile;
+   begin
+
+      for Ph in IOP_GRP1_Peripheral_Selection_Type'Range
+      loop
+         if Peripherals (Ph) = True
+         then
+            case Ph is
+               when GPIOA =>
+                  RCC_Periph.IOPENR.IOPAEN := 2#1#;
+                  UNUSED_Bit := RCC_Periph.IOPENR.IOPAEN;
+               when GPIOB =>
+                  RCC_Periph.IOPENR.IOPBEN := 2#1#;
+                  UNUSED_Bit := RCC_Periph.IOPENR.IOPBEN;
+               when GPIOC =>
+                  RCC_Periph.IOPENR.IOPCEN := 2#1#;
+                  UNUSED_Bit := RCC_Periph.IOPENR.IOPCEN;
+               when GPIOD =>
+                  RCC_Periph.IOPENR.IOPDEN := 2#1#;
+                  UNUSED_Bit := RCC_Periph.IOPENR.IOPDEN;
+               when GPIOE =>
+                  RCC_Periph.IOPENR.IOPEEN := 2#1#;
+                  UNUSED_Bit := RCC_Periph.IOPENR.IOPEEN;
+               when GPIOH =>
+                  RCC_Periph.IOPENR.IOPHEN := 2#1#;
+                  UNUSED_Bit := RCC_Periph.IOPENR.IOPHEN;
+            end case;
+         end if;
+      end loop;
+   end IOP_GRP1_EnableClock;
+
+end LL.Bus;
---
+++ src/ll-bus.ads
@@ -0,0 +1,70 @@
+------------------------------------------------------------------------------
+--  Copyright 2024, Emanuele Zarfati
+--
+--  Licensed under the Apache License, Version 2.0 (the "License");
+--  you may not use this file except in compliance with the License.
+--  You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+--  Unless required by applicable law or agreed to in writing, software
+--  distributed under the License is distributed on an "AS IS" BASIS,
+--  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--  See the License for the specific language governing permissions and
+--  limitations under the License.
+--
+------------------------------------------------------------------------------
+--
+--  Revision History:
+--    2024.03 E. Zarfati
+--       - First version
+--
+------------------------------------------------------------------------------
+
+package LL.Bus is
+   --  Bus low-level driver
+   --
+   --  Core bus control and peripheral clock activation and deactivation
+   --
+   --  Implementation notes:
+   --  - Based on source files:
+   --    - stm32l0xx_hal_driver:Inc/stm32l0xx_ll_bus.h
+   pragma Pure;
+
+   type APB1_GRP1_Peripheral_Type is
+      (TIM2, TIM3, TIM6, TIM7, LCD, WWDG, SPI2, USART2, LPUART1, USART4,
+         USART5, I2C1, I2C2, USB, CRS, PWR, DAC1, I2C3, LPTIM1);
+   --
+
+   type APB1_GRP1_Peripheral_Selection_Type is
+      array (APB1_GRP1_Peripheral_Type)
+      of Boolean
+      with Default_Component_Value => False;
+   --
+
+   type IOP_GRP1_Peripheral_Type is
+      (GPIOA, GPIOB, GPIOC, GPIOD, GPIOE, GPIOH)
+      with Default_Value => GPIOA;
+   --
+
+   type IOP_GRP1_Peripheral_Selection_Type is
+      array (IOP_GRP1_Peripheral_Type)
+      of Boolean
+      with Default_Component_Value => False;
+   --
+
+   ---------------------------------------------------------------------------
+   procedure APB1_GRP1_Enable_Clock (
+      Peripherals : APB1_GRP1_Peripheral_Selection_Type);
+   --  Enable APB1 peripherals clock
+   --
+   --  @param Peripherals
+
+   ---------------------------------------------------------------------------
+   procedure IOP_GRP1_EnableClock (
+      Peripherals : IOP_GRP1_Peripheral_Selection_Type);
+   --  Enable IOP peripherals clock
+   --
+   --  @param Peripherals
+
+end LL.Bus;
